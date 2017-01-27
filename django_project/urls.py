225214dce62e58b0c44f785e4537b9bb75314c96
Suraj Joshi <surajkincity@gmail.comit config --global user.email surajkincity@gmail.com>
2017-01-27 16:40:51 +0530

    W

--- django_project/urls.py
+++ django_project/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from django_project.views import home
+
 urlpatterns = [
     url(r'^$', home),
-    url(r'^admin/', include(admin.site.urls)),
+
+
 ]
